b9cab9af82a0091fd0ed82b8979eb304f90b2a32
unknown <galocoriamaio@gmail.com>
2025-04-09 11:49:06 -0300

    Hasta el 6

---
+++ Ejercicios.py
@@ -0,0 +1,57 @@
+#Ejercicio 1
+#for  x in range(1,101):
+  #  print(x)
+   # x=x+1
+
+#Ejercicio 2
+''' numero=int(input("Ingrese un numero entero:"))
+digito=0
+
+if numero ==0:
+     digito=1
+
+while  numero>0 :
+     
+     numero=numero//10
+    
+     digito=digito+1
+
+print(digito)  ''' 
+
+#Ejercicio 3
+'''numero1=int(input("Ingrese un numero entero:"))
+numero2=int(input("Ingrese otro numero:"))
+suma=0
+if numero1 > numero2:
+    numero1, numero2 = numero2, numero1
+
+while numero1 < numero2 - 1:
+    numero1 += 1
+    suma += numero1
+
+print(suma) ''' 
+
+#Ejercicio 4
+''' numero=int(input("Ingrese un numero:"))
+suma=numero
+while numero!=0:
+    numero=int(input("Ingrese un numero:"))
+    suma+=numero
+
+print(suma)'''
+
+#Ejercicio 5
+''' from random import *
+random=randint(0,9)
+numero=int(input("Ingrese un numero del 0 a 9:"))
+intentos=1
+while numero!= random :
+    numero=int(input("Ingrese un numero:"))
+    intentos=intentos+1
+
+print(f"Antes de adivinar el numero lo intento:{intentos} ") '''
+#Ejercicio 6
+'''for x in range(100,-1,-2):
+    print(x) '''
+
+#Ejercicio 7

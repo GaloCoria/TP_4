07efa25eae5a9cab513214942a07a0d9f0bc0052
unknown <galocoriamaio@gmail.com>
2025-04-09 14:42:54 -0300

    Terminado

--- Ejercicios.py
+++ Ejercicios.py
@@ -55,3 +55,48 @@ print(f"Antes de adivinar el numero lo intento:{intentos} ") '''
     print(x) '''
 
 #Ejercicio 7
+'''numero=int(input("Ingrese un numero entero positivo:"))
+suma=numero
+for x in range(0,numero):
+    suma+=x
+print(suma) '''
+#Ejercicio 8
+'''cantidad=0
+numpar=0
+numimpar=0
+numpositivo=0
+numnegativo=0
+while cantidad!=100:
+    numero=int(input("Ingresa un Numero Entero:"))
+    cantidad+=1
+    if numero %2==0:
+        numpar+=1
+    else: 
+        numimpar+=1
+    if numero >= 0 :
+        numpositivo+=1
+    else:
+        numnegativo+=1
+
+print(f"La cantidad de numeros pares es : {numpar}, la cantidad de numeros impares son : {numimpar}, la cantidad de numeros positivos son : {numpositivo} y la cantidad de numeros negativos son: {numnegativo}")'''
+
+#Ejercicio 9
+'''cantidad=0
+suma=0
+while cantidad !=2 :
+    numero=int(input("Ingresa un Numero Entero:"))
+    cantidad+=1
+    suma+=numero
+promedio=suma/cantidad
+print(f"El promedio de todos los numeros es: {promedio}") '''
+#Ejercicio 10
+numero = int(input("Ingresa un número entero: "))
+invertido = 0
+
+while numero > 0:
+    digito = numero % 10
+    invertido = invertido * 10 + digito
+    numero //= 10
+
+print("El número invertido es:", invertido)
+
